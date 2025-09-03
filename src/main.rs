7411970e2bc323ed3213ceca4d8d70a50ef8cff0
kageru <kageru@encode.moe>
2025-09-03 15:29:56 +0200

    add tests

--- src/main.rs
+++ src/main.rs
@@ -53,7 +53,7 @@ fn pop2(v: &mut Vec<f64>) -> Result<(f64, f64), &'static str> {
     Ok((a, b))
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 enum V {
     Value(f64),
     Add,
@@ -93,4 +93,41 @@ fn op(input: &str) -> IResult<&str, V> {
 
 fn float(input: &str) -> IResult<&str, V> {
     map(preceded(multispace0, double), V::Value).parse(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_test() {
+        assert_eq!(
+            parse("1 2+3-").expect("parsing failed").1[..],
+            [Value(1.0), Value(2.0), Add, Value(3.0), Sub],
+        );
+        assert_eq!(
+            parse("1 1-2--3").expect("parsing failed").1[..],
+            [Value(1.0), Value(1.0), Value(-2.0), Sub, Value(-3.0)],
+        );
+        assert_eq!(
+            parse(".5.5").expect("parsing failed").1[..],
+            [Value(0.5), Value(0.5)],
+        );
+    }
+
+    #[test]
+    fn evaluation_test() {
+        for (input, expectation) in [
+            ("1 2+3-", vec![0.0]),
+            ("40 2+6/7*", vec![49.0]),
+            ("5 2/3+3", vec![5.5, 3.0]),
+        ] {
+            let input = parse(input).expect("parsing failed").1;
+            let mut stack = Vec::new();
+            for v in input {
+                process(&mut stack, v).expect("processing failed");
+            }
+            assert_eq!(stack, expectation);
+        }
+    }
 }
